c7db335ce1458b66e48b95899175f8138c4402d9
Pit-Storm <33754750+Pit-Storm@users.noreply.github.com>
2020-11-14 22:25:07 +0000

    Added # TODO: s for own data

--- run_DRL.py
+++ run_DRL.py
@@ -23,16 +23,19 @@ def run_model() -> None:
     """Train the model."""
 
     # read and preprocess data
+    # TODO: Read in own Data
     data = preprocess_data()
     data = add_turbulence(data)
 
     # 2015/10/01 is the date that validation starts
     # 2016/01/01 is the date that real trading starts
     # unique_trade_date needs to start from 2015/10/01 for validation purpose
+    # TODO: this has to be a datetime range which all the dates except the first half (minus 3 months)
     unique_trade_date = data[(data.datadate > 20151001)&(data.datadate <= 20200707)].datadate.unique()
 
     # rebalance_window is the number of months (=Number/21) to retrain the model
-    # validation_window is the numebr of months (=Number/21) to validation the model and select for trading
+    # validation_window is the numeber of months (=Number/21) to validate the model and select for trading
+    # TODO: set this as a datetime element which counts the number of days.
     rebalance_window = 63
     validation_window = 63
     
